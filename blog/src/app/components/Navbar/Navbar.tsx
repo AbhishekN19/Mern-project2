01a3224bb5999aa08b39178fc1aac781be9a5dbe
Abhi <abhishekn190800@gmail.com>
2025-05-21 19:46:51 +0530

    progress on front end

---
+++ blog/src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoSearchSharp } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { FaCirclePlus } from "react-icons/fa6";
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-left'>
+
+        </div>
+        <div className='navbar-middle'></div>
+        <div className='navbar-right'></div>
+    </nav>
+  )
+}
+
+export default Navbar
